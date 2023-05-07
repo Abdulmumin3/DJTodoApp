6a577010451f9eaa02b19173f89a08fb208f3beb
Adesoye <abdulmumeenadesoye19@gmail.com>
2023-05-07 03:09:02 +0100

    Another commit

--- todo/settings.py
+++ todo/settings.py
@@ -86,11 +86,16 @@ WSGI_APPLICATION = "todo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+# "default": env.dj_db_url("DATABASE_URL")
+# }
+
 DATABASES = {
-"default": env.dj_db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
